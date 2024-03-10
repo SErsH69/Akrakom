401461f5533c75355eebf3ba0a792b0cd130abd6
Sergey Goncharov <sersh@MacBook-Pro-Sergey.local>
2024-03-11 03:43:46 +0500

    Публикация страницы Страница карточки товара

--- src/blocks/modules/main_tabs/main_tabs.js
+++ src/blocks/modules/main_tabs/main_tabs.js
@@ -4,9 +4,14 @@ const MainTabs = class MainTabs {
         document.addEventListener('DOMContentLoaded', function() {
             var tabs = document.querySelectorAll('.main_tabs__tab');
             var contents = document.querySelectorAll('.main_tabs__content');
+            var tabsContainer = document.querySelector('.main_tabs__tabs');
     
             tabs.forEach(function(tab, index) {
                 tab.addEventListener('click', function() {
+                    // Проверяем, имеет ли кликнутая вкладка класс isActive
+                    var isActive = tab.classList.contains('isActive');
+    
+                    // Удаляем класс isActive у всех вкладок и их содержимого
                     tabs.forEach(function(tab) {
                         tab.classList.remove('isActive');
                     });
@@ -14,8 +19,16 @@ const MainTabs = class MainTabs {
                         content.classList.remove('isActive');
                     });
     
+                    // Добавляем класс isActive только кликнутой вкладке и её содержимому
                     tab.classList.add('isActive');
                     contents[index].classList.add('isActive');
+    
+                    // Проверяем, имеет ли кликнутая вкладка класс isActive
+                    if (isActive) {
+                        tabsContainer.classList.add('isShow');
+                    } else {
+                        tabsContainer.classList.remove('isShow');
+                    }
                 });
             });
         });
